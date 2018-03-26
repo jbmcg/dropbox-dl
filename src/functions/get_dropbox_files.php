e3e18e99dabaea17a8089b9f09b246bb325102cc
Josh McGrath <josh.bernard.mcgrath@gmail.com>
2018-03-26 16:03:53 -0300

    handle updated dropbox preview page structure III

--- src/functions/get_dropbox_files.php
+++ src/functions/get_dropbox_files.php
@@ -4,35 +4,34 @@ function get_dropbox_files($url, &$files, $recursive = true, $validExtensions = 
 {
     println('Checking %s', $url);
     $contents = file_get_contents($url);
-    $start = strstr($contents, 'InitReact.mountComponent(');
+    $search = '.responseReceived("';
+    $start = strstr($contents, $search);
     list($line) = explode(PHP_EOL, $start);
-    $lineLength = strlen($line);
-    $jsonStart = strpos($line, '{');
-    $jsonEnd = strrpos($line, '},');
-    $json = substr($line, $jsonStart, $jsonEnd - $jsonStart + 1) . '}';
-    $result = json_decode($json, true);
-    $data = $result['props']['contents'];
+    $jsonStart = strpos($line, $search) + strlen($search);
+    $jsonEnd = strrpos($line, '")');
+    $json = substr($line, $jsonStart, $jsonEnd - $jsonStart);
+    $jsonStr = json_decode('"' . $json . '"', true);
+    $result = json_decode($jsonStr, true);
 
-    if ($recursive && array_key_exists('folders', $data)) {
-        foreach ($data['folders'] as $folderObj) {
-            get_dropbox_files($folderObj['href'], $files, $recursive, $validExtensions,
-                sprintf('/%s/', basename(strtok($folderObj['href'], '?'))));
-        }
-    }
-
-    if (array_key_exists('files', $data)) {
-        foreach ($data['files'] as $fileObj) {
-            $add = replace_query_params($fileObj['href'], ['dl' => 1]);
-            if (!array_key_exists($folder, $files)) {
-                $files[$folder] = array();
-            }
-            if (count($validExtensions) > 0) {
-                $ext = pathinfo(strtok($add, '?'), PATHINFO_EXTENSION);
-                if (in_array(strtolower($ext), $validExtensions) && !in_array($add, $files[$folder])) {
+    if (array_key_exists('entries', $result)) {
+        foreach ($result['entries'] as $entry) {
+            $isDir = filter_var($entry['is_dir'], FILTER_VALIDATE_BOOLEAN);
+            if ($recursive && $isDir) {
+                get_dropbox_files($entry['href'], $files, $recursive, $validExtensions,
+                    sprintf('/%s/', basename(strtok($entry['href'], '?'))));
+            } elseif (!$isDir) {
+                $add = replace_query_params($entry['href'], ['dl' => 1]);
+                if (!array_key_exists($folder, $files)) {
+                    $files[$folder] = array();
+                }
+                if (count($validExtensions) > 0) {
+                    $ext = pathinfo(strtok($add, '?'), PATHINFO_EXTENSION);
+                    if (in_array(strtolower($ext), $validExtensions) && !in_array($add, $files[$folder])) {
+                        $files[$folder][] = $add;
+                    }
+                } elseif (!in_array($add, $files[$folder])) {
                     $files[$folder][] = $add;
                 }
-            } elseif (!in_array($add, $files[$folder])) {
-                $files[$folder][] = $add;
             }
         }
     }
